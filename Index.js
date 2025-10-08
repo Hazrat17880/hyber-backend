3b4aa3f717dbf5d14a5eb80a8a0633a346855769
Hazrat 17880 <hazrat17780@gmail.com>
2025-10-08 21:10:42 +0500

    added new code

--- Index.js
+++ Index.js
@@ -9,7 +9,7 @@ import { DBConnections } from "./Database/Db.js";
 import { Route } from "./Routes/User.js";
 
 const allowedOrigins = [
-  "https://frontend-d640oprse-hazrat-usmans-projects.vercel.app",
+  "https://frontend-24klq6ey0-hazrat-usmans-projects.vercel.app",
   "http://localhost:5173", // for local dev with Vite
 ];
 
